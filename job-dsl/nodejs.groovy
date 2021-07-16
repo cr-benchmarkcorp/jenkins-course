9daf3b22f1ae3f6eac185d2eb9f69b930bc40ac5
chadwick.radunske <chadwick.radunske@benchmarkcorp.com>
2021-07-16 15:47:30 -0400

    changed git address to my Benchmark repo

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/cr-benchmark/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
@@ -10,7 +10,7 @@ job('NodeJS example') {
     }
     wrappers {
         nodejs('cbr-nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+                             // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
         shell("npm install")
